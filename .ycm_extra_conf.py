11b9a844adcf168e8d210c976a392ed2b4165040
ColinAllenDev <colinallen.dev@gmail.com>
2021-05-21 15:42:16 -0400

    Removed my gl syntax highlighter for another

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -6,32 +6,29 @@ import ycm_core
 import re
 
 C_BASE_FLAGS = [
-        '-Wall',
-        '-Wextra',
-        '-Werror',
-        '-Wno-long-long',
-        '-Wno-variadic-macros',
-        '-fexceptions',
-        '-ferror-limit=10000',
-        '-DNDEBUG',
+#        '-Werror',
+#        '-Wno-long-long',
+#        '-Wno-variadic-macros',
+#        '-fexceptions',
+#        '-ferror-limit=10000',
+#        '-DNDEBUG',
         '-std=c11',
         '-I/usr/lib/',
         '-I/usr/include/'
         ]
 
 CPP_BASE_FLAGS = [
-        '-Wall',
-        '-Wextra',
-        '-Wno-long-long',
-        '-Wno-variadic-macros',
-        '-fexceptions',
-        '-ferror-limit=10000',
-        '-DNDEBUG',
+#        '-Werror',
+#        '-Wno-long-long',
+#        '-Wno-variadic-macros',
+#        '-fexceptions',
+#        '-ferror-limit=10000',
+#        '-DNDEBUG',
         '-std=c++1z',
         '-xc++',
         '-I/usr/lib/',
         '-I/usr/include/',
-        '-I/home/colin/Documents/Projects/GameEngine/include/'
+        '-I/home/colin/Documents/Projects/Crypt/include/'
         ]
 
 C_SOURCE_EXTENSIONS = [
---
+++ README.md
@@ -0,0 +1,2 @@
+# DotFiles - Arch, Ubuntu, Vim, Etc 
+These are my dotfiles for my Arch setup with Xmonad, Ubuntu configs
